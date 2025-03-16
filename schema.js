097784a081e3c337c0e1dc10fcd4ee978e6c2cc2
Youcef Kesbi <ykesbi@gmail.com>
2025-03-16 13:52:20 +0300

    update

--- schema.js
+++ schema.js
@@ -1,12 +1,17 @@
 export const typeDefs = `#graphql
     type Game {
         id: ID!,
-        title: String,
-        platform: [String]
+        title: String!,
+        platform: [String!]!
     }
     type Review {
         id: ID!,
         rating: Int!,
         content: String!
     }
-`;
+    type Author {
+        id: ID!,
+        name: String!,
+        verified: Boolean!
+    }
+`
