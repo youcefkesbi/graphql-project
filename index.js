8268559e0e82dc3ed388f0d740a199b4f226d850
Youcef Kesbi <ykesbi@gmail.com>
2025-03-16 13:42:20 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+// server setup
+
+const server = new ApolloServer({
+    // typeDefs
+    // resolvers
+});
+
+const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+});
+
+console.log(`🚀 Server ready at port`, 4000);
---
+++ schema.js
@@ -0,0 +1,8 @@
+export const typeDefs = `#graphql
+    type Game {
+        id: ID!,
+        title: String,
+        platform: [String]
+    }
+
+`;
